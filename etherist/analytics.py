1de0e3947204a41c03bc24f4188d2f783e260564
Dominiek Ter Heide <dominiek@bottlenose.com>
2016-03-09 18:42:26 -0800

    Twekas

--- etherist/analytics.py
+++ etherist/analytics.py
@@ -20,12 +20,65 @@ class Analytics:
     def get_ticker(self):
         return self.data['last_ticker']
 
-    def ticker_time_series(self, limit=10000, interval=60, time_range=3600, field='last', operations=['first', 'last', 'max', 'min'], as_datetime=False):
+    def ticker_time_series(self, limit=10000, now_ts=0, interval=60, time_range=3600, field='last', operations=['first', 'last', 'max', 'min'], as_datetime=False):
         events = []
         for line in tailer.tail(open(DATA_DIR + "/ticker.jsons"), limit):
             events.append(json.loads(line))
         now = int(math.ceil(time.time()))
+        if now_ts > 0:
+            now = int(math.ceil(now_ts / 1000.0))
         return self._compute_time_series(events, now - time_range, now, interval, field, operations, as_datetime)
+
+    def volume_time_series(self, limit=10000, now_ts=0, interval=60, trade_type='ask', time_range=3600, field='amount', operations=['sum'], min_amount=0, as_datetime=False):
+        events = []
+        for line in tailer.tail(open(DATA_DIR + "/trades.jsons"), limit):
+            event = json.loads(line)
+            events.append(event)
+        now = int(math.ceil(time.time()))
+        if now_ts > 0:
+            now = int(math.ceil(now_ts / 1000.0))
+        time_series = []
+        from_ts = now - time_range
+        to_ts = now
+        for ts in range(from_ts, to_ts, interval):
+            values = []
+            for event in events:
+                event_ts = event['_ts'] / 1000
+                #print('ts', ts, event_ts, event_ts >= ts, event_ts <= (ts+interval))
+                data = event.get('data', None)
+                if data == None:
+                    continue
+                if trade_type != 'all' and data['type'] != trade_type:
+                    continue
+                if event_ts >= ts and event_ts <= (ts + interval):
+                    value = float(data.get(field, 0.0))
+                    if (value < min_amount):
+                        continue
+                    value = 1
+                    values.append(value)
+            data_points = []
+            for operation in operations:
+                data_point = 0
+                if len(values) == 0:
+                    data_point = 0
+                elif operation == 'avg':
+                    data_point = sum(values) / len(values)
+                elif operation == 'sum':
+                    data_point = sum(values)
+                elif operation == 'max':
+                    data_point = max(values)
+                elif operation == 'min':
+                    data_point = min(values)
+                elif operation == 'first':
+                    data_point = values[0]
+                elif operation == 'last':
+                    data_point = values[len(values)-1]
+                data_points.append(float(data_point))
+            if as_datetime == True:
+                time_series.append([datetime.fromtimestamp(ts)] + data_points)
+            else:
+                time_series.append([ts] + data_points)
+        return time_series
 
     def track_ticker(self):
         for line in tailer.follow(open(DATA_DIR + "/ticker.jsons")):
@@ -39,9 +92,8 @@ class Analytics:
             values = []
             for event in events:
                 event_ts = event['_ts'] / 1000
-                #print('ts', ts, event_ts, event_ts >= ts, event_ts <= (ts+interval))
                 if event_ts >= ts and event_ts <= (ts + interval):
-                    values.append(event[field])
+                    values.append(float(event[field]))
             data_points = []
             for operation in operations:
                 data_point = 0
--- etherist/visualize.py
+++ etherist/visualize.py
@@ -4,14 +4,15 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 import time
 from matplotlib.dates import DateFormatter, epoch2num
-from matplotlib.finance import candlestick, plot_day_summary, candlestick2
+from matplotlib.finance import candlestick_ochl, volume_overlay
+
 from tzlocal import get_localzone
 
 DATA_DIR = os.path.dirname(os.path.realpath(__file__)) + '/../data'
 
 class Visualize:
 
-    def candlestick_chart(self, time_series, interval=30, show=False, filename='candlestick.png'):
+    def candlestick_chart(self, time_series, interval=30, show=False, filename='candlestick.png', volume_overlay=None):
         tz = get_localzone()
         xfmt = DateFormatter('%H:%M:%S', tz=tz.localize(datetime.now()).tzinfo)
         adjusted_time_series = []
@@ -25,12 +26,42 @@ class Visualize:
         ax.xaxis.set_major_formatter(xfmt)
         #ax.xaxis.set_major_formatter(weekFormatter)
         days_interval = interval / 86400.0
-        candlestick(ax, adjusted_time_series, width=days_interval, colorup='green', colordown='red', alpha=0.9)
+        candlestick_ochl(ax, adjusted_time_series, width=days_interval, colorup='green', colordown='red', alpha=0.9)
 
         ax.xaxis_date(tz=tz.zone)
-        ax.autoscale_view(scaley=False)
+        ax.autoscale_view()
         ax.grid(True)
         plt.setp( plt.gca().get_xticklabels(), rotation=45, horizontalalignment='right')
+
+        if volume_overlay != None:
+            # Add a seconds axis for the volume overlay
+            ax2 = ax.twinx()
+
+            # set the position of ax2 so that it is short (y2=0.32) but otherwise the same size as ax
+            ax2.set_position(matplotlib.transforms.Bbox([[0.125,0.1],[0.9,0.32]]))
+
+            # get data from candlesticks for a bar plot
+            dates = [x[0] for x in adjusted_time_series]
+            dates = np.asarray(dates)
+            volume = [x[1] for x in volume_overlay]
+            volume = np.asarray(volume)
+
+            ax2.bar(dates,volume,color='#cccccc',width=days_interval,align='center')
+
+            #scale the x-axis tight
+            ax2.set_xlim(min(dates),max(dates))
+            # the y-ticks for the bar were too dense, keep only every third one
+            yticks = ax2.get_yticks()
+            ax2.set_yticks(yticks[::3])
+
+            ax2.yaxis.set_label_position("right")
+            ax2.set_ylabel('Volume', size=20)
+
+            # format the x-ticks with a human-readable date.
+            #xt = ax.get_xticks()
+            #new_xticks = [datetime.date.isoformat(num2date(d)) for d in xt]
+            #ax.set_xticklabels(new_xticks,rotation=45, horizontalalignment='right')
+
         if show:
             plt.show()
         else:
